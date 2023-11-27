5891e48d48c70933a038ae1dce4f8157375e1901
Nguyen Xuan Hung <nguyenxuanhung@mac.local>
2023-11-27 21:38:46 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { Canvas, Fill } from '@shopify/react-native-skia';
+
+function App(): JSX.Element {
+
+  return (
+    <Canvas style={{ flex: 1 }}>
+      <Fill color={'red'} />
+    </Canvas>
+  );
+}
+
+export default App;
